ee5836d3624272e2be2daf523d4e5bd421781b25
udscoder <udscoder@gmail.com>
2024-06-29 02:09:53 +0500

    verstka

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { SearchIcon } from "./icons/SearchIcon";
+import { PlayCircleIcon } from "./icons/PlayCircleIcon";
+import { ChevronDownIcon } from "./icons/ChevronDownIcon";
+import { useNavigate } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App: FC = () => {
+    const navigate = useNavigate()
+    const pathname = window.location.pathname
+
+    const headerParagraphs = [
+        {
+            id: 1,
+            title: 'Главная',
+            route: '/'
+        },
+        {
+            id: 2,
+            title: 'О нас',
+            route: '/about'
+        },
+        {
+            id: 3,
+            title: 'Продукты',
+            route: '/products'
+        },
+        {
+            id: 4,
+            title: 'Контакты',
+            route: '/contacts'
+        },
+        {
+            id: 5,
+            title: 'Новости',
+            route: '/news'
+        }
+    ]
+
+    return (
+        <div className="main">
+            <img src="/images/background_main.png" alt="background" className='main__background' />
+
+            <div className='section'>
+                <div className='header'>
+                    <img src="/images/main_logo.png" alt="logo" className='header__logo' />
+
+                    <div className='header__right'>
+                        <div className='header__paragraphs'>
+                            { headerParagraphs.map((paragraph, idx) => (
+                                <p
+                                    key={idx}
+                                    className={ `${ pathname === paragraph.route ? 'header__paragraphs-active' : '' }` }
+                                    onClick={ () => navigate(paragraph.route) }
+                                >
+                                    { paragraph.title }
+                                </p>
+                            )) }
+                        </div>
+
+                        <label className='header-search'>
+                            <input type="search" placeholder='Поиск...' />
+
+                            <SearchIcon />
+                        </label>
+
+                        <div className='header-select'>
+                            <h1>Ру</h1>
+
+                            <ChevronDownIcon />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='content'>
+                    <h1 className='content__title'>
+                        Экологически чистые продукты из богатой флоры
+                        <span>Узбекистана</span>
+                    </h1>
+
+                    <p className='content__paragraph'>
+                        Страна Шелкового пути, древней сети торговых маршрутов, была местом культурного обмена и
+                        глубоких знаний. Опираясь на богатое историческое наследие Узбекистана и стремление к
+                        инновациям, в 2018 году в самом сердце Центральной Азии было создано современное предприятие,
+                        целью которого является использование экологически чистых, натуральных местных природных
+                        богатств — цветов, листьев, кожуры, семян, корней дикорастущих лекарственных растений, фруктов и
+                        овощей
+                    </p>
+
+                    <div className="content__buttons">
+                        <button className="content__buttons-button">
+                            <p>Связаться с нами</p>
+                        </button>
+                        <button className="content__buttons-button">
+                            <p>Смотреть о нас</p> <PlayCircleIcon />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/icons/SearchIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const SearchIcon = () => {
+    return (
+        <svg className="svg-item" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
+            <path d="M17 17L21 21" stroke="#ffffff" strokeWidth="2" stroke-linecap="round"
+                  stroke-linejoin="round" className=""></path>
+            <path
+                d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
+                stroke="#ffffff" stroke-width="2" stroke-linecap="round" strokeLinejoin="round"
+                className=""></path>
+        </svg>
+    )
+}
